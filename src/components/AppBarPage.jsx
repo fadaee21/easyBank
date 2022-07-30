e782c94f96003a77cf8d0988483d89be2b804dbd
fadarash <fadarash@gmail.com>
2022-07-30 13:18:36 +0430

    create close and burger button

--- src/components/AppBarPage.jsx
+++ src/components/AppBarPage.jsx
@@ -5,13 +5,12 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import { MenuButton } from '../styles/appbar';
-import { EasyBankLogo } from '../assets/allSVGicons';
+import { BurgerButton, CloseButton, EasyBankLogo } from '../assets/allSVGicons';
 
 
 
@@ -22,7 +21,6 @@ const pages = ['Home', 'About', 'Contact', 'Blog', 'Careers'];
 export const AppBarPage = () => {
 
     const [anchorElNav, setAnchorElNav] = React.useState(null);
-
 
     const handleOpenNavMenu = (event) => {
         setAnchorElNav(event.currentTarget);
@@ -38,7 +36,7 @@ export const AppBarPage = () => {
     return (
         <AppBar position="static" elevation={0} sx={{ position: "relative", zIndex: 10 }} >
             <Container maxWidth="lg"   >
-                <Toolbar disableGutters sx={{ height: '80px', margin:{md: "0 3.2rem"}, justifyContent: { xs: "space-between" } }}>
+                <Toolbar disableGutters sx={{ height: '80px', margin: { md: "0 3.2rem" }, justifyContent: { xs: "space-between" } }}>
                     <EasyBankLogo color={"#2D314D"} sx={{ display: { xs: 'none', md: 'flex' } }} />
                     <Box sx={{ flexGrow: { xs: 0, md: 1 }, display: { xs: 'flex', md: 'none' } }}>
                         <IconButton
@@ -47,11 +45,10 @@ export const AppBarPage = () => {
                             aria-controls="menu-appbar"
                             aria-haspopup="true"
                             onClick={handleOpenNavMenu}
-
                         >
-                            <MenuIcon />
+                            {anchorElNav ? <CloseButton /> : <BurgerButton />}
                         </IconButton>
-                        <Menu 
+                        <Menu
                             id="menu-appbar"
                             anchorEl={anchorElNav}
                             anchorOrigin={{
@@ -65,13 +62,10 @@ export const AppBarPage = () => {
                             }}
                             open={Boolean(anchorElNav)}
                             onClose={handleCloseNavMenu}
-                            sx={{
-                                display: { xs: 'block', md: 'none' },
-
-                            }}
+                            sx={{ display: { xs: 'block', md: 'none' }, }}
                         >
                             {pages.map((page) => (
-                                <MenuItem key={page} onClick={handleCloseNavMenu} sx={{ justifyContent: 'center'}} >
+                                <MenuItem key={page} onClick={handleCloseNavMenu} sx={{ justifyContent: 'center' }} >
                                     <Typography textAlign="center">{page}</Typography>
                                 </MenuItem>
                             ))}
